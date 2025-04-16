bf5784174a2f6ff9a417f8fd97c003f0b2f142f2
Cloudman@Home <cloudman4416@gmail.com>
2025-04-16 17:11:58 +0200

    Update Map 2 Priv

--- 2142948266/13883279773.lua
+++ 2142948266/13883279773.lua
@@ -29,6 +29,7 @@ for i, v in workspace.Mobs.Bosses:GetDescendants() do
         bosses[info["Name"]] = info["Npc_Spawning"]["Spawn_Locations"][1]
     end
 end
+
 local temp = {
     workspace.Mobs.Bandits.Zone1.Boss;
     workspace.Mobs.Bandits.Zone2.Kaden;
