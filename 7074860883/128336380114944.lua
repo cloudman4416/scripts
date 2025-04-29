5b8f35d2613b9c42fa48fce44abb5261a1ab8908
Cloudman@Home <cloudman4416@gmail.com>
2025-04-29 22:11:44 +0200

    Update Arise Crossover Dungeon

--- 7074860883/128336380114944.lua
+++ 7074860883/128336380114944.lua
@@ -140,7 +140,7 @@ Tabs["Auto Farm"]:AddToggle("tAutoMobs", {
                                             ["Event"] = "Attack",
                                             ["Enemy"] = target.Name
                                         },
-                                        [2] = "\5"
+                                        [2] = "\8"
                                     })
                                     task.wait(0.3)
                                 end
