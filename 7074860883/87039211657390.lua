dcd05bc4302560a7d1272009697882b12764d2b8
Cloudman@Home <cloudman4416@gmail.com>
2025-05-01 19:49:02 +0200

    Update Arise Crossover Main

--- 7074860883/87039211657390.lua
+++ 7074860883/87039211657390.lua
@@ -205,7 +205,7 @@ Tabs["Auto Farm"]:AddToggle("tAutoMobs", {
                                 continue
                             end
                             if mobinfo[v:GetAttribute("Model")].Name == options["dMobSelect"].Value and (options["tFarmBrute"].Value or not mobinfo[v:GetAttribute("Id")].TypeG) then
-                                tweento(v.CFrame * CFrame.new(8, 0, 0) * CFrame.Angles(0, math.rad(90), 0)).Completed:Wait()
+                                tpto(v.CFrame * CFrame.new(8, 0, 0) * CFrame.Angles(0, math.rad(90), 0))
                                 task.wait(0.3)
                                 local target = clientMobs:WaitForChild(v.Name, 2)
                                 if not target then continue end
