795b0b924312f3cc4e066329a3f097b033e87760
cloudman4416 <134954873+cloudman4416@users.noreply.github.com>
2025-07-29 23:27:01 +0200

    Update base.lua

--- 2142948266/base.lua
+++ 2142948266/base.lua
@@ -2,7 +2,7 @@ repeat task.wait() until game:IsLoaded()
 local Library = loadstring(game:HttpGetAsync("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
---cache test
+
 local options = Library.Options
 warn("---------------------------------")
 -- SERVICES
