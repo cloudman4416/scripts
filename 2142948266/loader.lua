736f479cd6a4873884390c0da9a4327fee0c2d19
Cloudman@Home <cloudman4416@gmail.com>
2025-08-29 18:16:37 +0200

    Update LOADER

--- 2142948266/loader.lua
+++ 2142948266/loader.lua
@@ -48,6 +48,77 @@ download("https://raw.githubusercontent.com/cloudman4416/scripts/refs/heads/main
 local baseUrl = `https://raw.githubusercontent.com/cloudman4416/scripts/refs/heads/main/{game.GameId}/{game.PlaceId}.lua`
 local base64url = `https://api.github.com/repos/cloudman4416/scripts/contents/{game.GameId}/{game.PlaceId}.lua?ref=main`
 
+local function checkKey(key)
+	local response = HttpService:JSONDecode(game:HttpGet("https://work.ink/_api/v2/token/isValid/" .. key or "abcdefg"))
+	if response.valid then
+		writefile("CloudHub/Key", response.info.token)
+		return true
+	else
+		
+		return false
+	end
+end
+
+makefolder("CloudHub")
+
+if not (isfile("CloudHub/Key") and checkKey(readfile("CloudHub/Key"))) then
+	pcall(function()
+		local Fluent = loadstring(game:HttpGet("https://github.com/cloudman4416/Fluent_Clone/releases/latest/download/main.lua"))()
+		local options = Fluent.Options
+
+		local Window = Fluent:CreateWindow({
+			Title = "Key System",
+			SubTitle = "CloudHub",
+			TabWidth = 160,
+			Size = UDim2.fromOffset(580, 340),
+			Acrylic = false,
+			Theme = "Obsidian",
+			MinimizeKey = Enum.KeyCode.LeftControl
+		})
+
+		local Tabs = {
+			KeySys = Window:AddTab({ Title = "Key System", Icon = "key" }),
+		}
+
+		local Entkey = Tabs.KeySys:AddInput("Input", {
+				Title = "Enter Key",
+				Description = "Enter Key Here",
+				Default = "",
+				Placeholder = "Enter key…",
+				Numeric = false,
+				Finished = false,
+		})
+
+		local Checkkey = Tabs.KeySys:AddButton({
+			Title = "Check Key",
+			Description = "Enter Key before pressing this button",
+			Callback = function()
+				if checkKey(options.Input.Value) then
+					Fluent:Destroy()
+				else
+					Fluent:Notify({
+						Title = "Key System",
+						Content = "Invalide Key",
+						Duration = 20
+					})
+				end
+			end
+		})
+
+		local Getkey = Tabs.KeySys:AddButton({
+			Title = "Get Key",
+			Description = "Get Key here",
+			Callback = function()
+				setclipboard("https://workink.net/1Sgk/n0noofu9")
+			end
+		})
+
+		Window:SelectTab(1)
+
+		while Fluent.Unloaded == false do task.wait() end
+	end)
+end
+
 local succ, err;
 while not succ do
     succ, err = pcall(function()
