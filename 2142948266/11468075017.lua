4f7de2a5046183b3384a5071a0185bb8576a4bb4
Cloudman@Home <cloudman4416@gmail.com>
2025-10-09 23:26:58 +0200

    Update DUNGEON

--- 2142948266/11468075017.lua
+++ 2142948266/11468075017.lua
@@ -6,6 +6,16 @@ if not options then return true end
 -- SERVICES
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
+local exec = identifyexecutor()
+local isBadExec = false
+if table.find({"Xeno", "Solara"}, exec) then
+    local data = loadstring(game:HttpGet("https://raw.githubusercontent.com/cloudman4416/scripts/refs/heads/main/2142948266/modular.lua"), "modular")()
+    isBadExec = true
+	getgenv().require = function(source)
+        return data[source:GetFullName()]
+    end
+end
+
 -- VARS
 
 for i, v in pairs(ReplicatedStorage.Ouwigahara.Bosses:GetChildren()) do
